7df8efa1d445dd3fb2b4aa230ed4e84b4b19b057
Extravi <98912029+Extravi@users.noreply.github.com>
2022-09-28 11:11:17 -0400

    Add files via upload

--- donate.php
+++ donate.php
@@ -16,6 +16,15 @@
                <p>41dGQr9EwZBfYBY3fibTtJZYfssfRuzJZDSVDeneoVcgckehK3BiLxAV4FvEVJiVqdiW996zvMxhFB8G8ot9nBFqQ84VkuC</p>
                <img src="static/images/xmr.png" alt="xmr qr code" width="150" height="150"/>
         </div>
+        <div class="misc-container">
+              <h1>Donate to librex.extravi.dev instance</h1>
+               <h3>Bitcoin (BTC):</h3>
+               <p>bc1qnwc9guv3t98sspf4schl082tqzy54m8ztdqqxg</p>
+               <img src="static/images/btc_instance.png" alt="btc qr code" width="150" height="150"/>
+               <h3>Monero (XMR):</h3>
+               <p>45W4D3rwsK5ifhkkvYtw4dZRzRwiyZhMqKR7jLSMNxBxTzXeJ4xfJYH5JQb3y5tYUJ2GEDMoSY9T6eemMN3hCEsALXHGLVj</p>
+               <img src="static/images/xmr_instance.png" alt="xmr qr code" width="150" height="150"/>
+        </div>
         <style>.footer-container{position:fixed;}</style>
 
 <?php require "misc/footer.php"; ?>
