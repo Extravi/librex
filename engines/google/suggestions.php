9f23e52097ae176ab6d0736aefdac96542c5b45b
Extravi <98912029+Extravi@users.noreply.github.com>
2022-11-13 14:22:15 -0500

    Update suggestions.php

--- engines/google/suggestions.php
+++ engines/google/suggestions.php
@@ -2,7 +2,7 @@
     function get_suggestions_results($query) {
         global $config;
         $query_encoded = urlencode($query);
-        $url = "http://suggestqueries.google.com/complete/search?output=firefox&q=$query_encoded";
+        $url = "https://suggestqueries.google.com/complete/search?output=firefox&q=$query_encoded";
 
         // Make request
         $ch = curl_init($url);
